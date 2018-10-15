a67e893ee556ca39163cd4045320149192a1f5f1
boyd ndonga <boydbugle@gmail.com>
2018-10-16 02:12:54 +0300

    create app instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,23 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+db = PyMongo()
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configuration
+    app.config.from_object(config_options[config_name])
+
+    # initialise app
+    db.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+
+    return app
+    app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
